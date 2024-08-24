142f359ce84c8dbee8d44247d762e9b1c78fdab2
Ignacio <ignaciosalazar986@gmail.com>
2024-08-24 16:51:59 -0300

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignInComponent } from './auth/signIn/signIn';
 
 const routes: Routes = [
   { path: '', redirectTo: '/start', pathMatch: 'full' },
   { path: 'start', component: DashboardComponent },
   { path: 'log-in', component: LoginComponent },
+  { path: 'sign-in', component: SignInComponent },
 ];
 
 @NgModule({
